a887bc32e670cea0cc27efa2ee3352a25c771c59
Kojo Kumi <kojokumi16.kk@gmail.com>
2023-07-05 15:47:01 -0700

    writing class 2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,12 +1,24 @@
-/// <reference path="./crud.d.ts" />
-import { RowID, RowElement } from './interface';
-import * as CRUD from './crud';
-
-const row: RowElement = {
-  firstName: 'Guillaume',
-  lastName: 'Salva',
+export const cpp: Subjects.Cpp = new Subjects.Cpp();
+export const java: Subjects.Java = new Subjects.Java();
+export const react: Subjects.React = new Subjects.React();
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'Dennis',
+  lastName: 'Ritchie',
+  experienceTeachingC: 10,
 };
-const newRowID: RowID = CRUD.insertRow(row);
-const updatedRow: RowElement = {...row, age: 23};
-CRUD.updateRow(newRowID, updatedRow);
-CRUD.deleteRow(newRowID);
+
+console.log('C++');
+cpp.setTeacher = cTeacher;
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log('Java');
+java.setTeacher = cTeacher;
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+react.setTeacher = cTeacher;
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
+
