342f7341863fc0ae9bfae176c46ee131f4a94850
Kojo Gyan Kumi <kojokumi16.kk@gmail.com>
2023-07-09 12:35:25 +0000

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 /**
  * Retrieves a list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Kojo Kumi <https://github.com/kojoawesome>
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export default function getListStudents() {
